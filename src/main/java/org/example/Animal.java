660856569fe5a21883e10483b587d6e69d14b10c
Valentina Taborda <valentina18tabordarico@gmail.com>
2023-02-21 20:50:36 -0500

    se crea proyecto zoologico

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Animal {
+
+    //Atributos
+    Integer edad;
+    String nombre;
+    String especie;
+    Double peso;
+    Double estatura;
+    String colorPredominante;
+    Boolean genero;
+    Integer numeroPatas;
+    String dieta;
+    String clasificacion; //Domestico o salvaje
+
+
+    //Metodos especiales
+
+    public Animal() {
+    }
+
+    public Animal(Integer edad, String nombre, String especie, Double peso, Double estatura, String colorPredominante, Boolean genero, Integer numeroPatas, String dieta, String clasificacion) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.colorPredominante = colorPredominante;
+        this.genero = genero;
+        this.numeroPatas = numeroPatas;
+        this.dieta = dieta;
+        this.clasificacion = clasificacion;
+    }
+
+    //Metodos ordinarios
+}
